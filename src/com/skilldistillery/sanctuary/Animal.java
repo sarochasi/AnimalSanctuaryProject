1ea3afcc0a073d826b146d4be4f052903b717c9b
Sarocha <rocha.right007@gmail.com>
2024-06-28 16:58:22 -0600

    Add name property to animal

--- src/com/skilldistillery/sanctuary/Animal.java
+++ src/com/skilldistillery/sanctuary/Animal.java
@@ -1,5 +1,15 @@
 package com.skilldistillery.sanctuary;
 
 public class Animal {
+	private String name;
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 }
